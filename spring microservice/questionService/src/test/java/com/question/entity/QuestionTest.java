f1fede39e8203f15c056812c4e97a2a908c0c534
priyansh-parmar-1 <priyanshparmar199@gmail.com>
2024-10-10 11:59:59 +0530

    Add builder for entities.

--- spring microservice/questionService/src/test/java/com/question/entity/QuestionTest.java
+++ spring microservice/questionService/src/test/java/com/question/entity/QuestionTest.java
@@ -3,22 +3,37 @@ package com.question.entity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class QuestionTest {
 
     @DisplayName("Question model test")
     @Test
-    void testQuestion(){
+    void testQuestion() {
         Question questionEmpty = new Question();
-        Question question = new Question(1L,"test",1L);
+        Question question = new Question(1L, "test", 1L);
         questionEmpty.setId(2L);
         questionEmpty.setQuestion("test1");
         questionEmpty.setQuizId(2L);
 
-        assertEquals(1L,question.getId());
-        assertEquals("test",question.getQuestion());
-        assertEquals(1L,question.getQuizId());
+        assertEquals(1L, question.getId());
+        assertEquals("test", question.getQuestion());
+        assertEquals(1L, question.getQuizId());
+
+    }
+
+    @Test
+    void testQuestionBuilder() {
+        Question questionEmpty = new Question();
+        Question question = Question.builder()
+                .id(1L)
+                .question("test")
+                .quizId(1L)
+                .build();
+
+        assertEquals(1L, question.getId());
+        assertEquals("test", question.getQuestion());
+        assertEquals(1L, question.getQuizId());
 
     }
 }
--- spring microservice/quizService/src/main/java/com/quiz/entity/Question.java
+++ spring microservice/quizService/src/main/java/com/quiz/entity/Question.java
@@ -4,12 +4,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Question {
 
     private Long id;
